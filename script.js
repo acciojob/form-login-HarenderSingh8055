f19fd899320d2604dd5efbf6382c61fdb4138abd
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-16 13:30:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+    // e.preventDefault();
+	let firstName = document.querySelector("[name=fname]").value;
+	let lastName = document.querySelector("[name=lname]").value;
+    alert(`${firstName} ${lastName}`)
 }
